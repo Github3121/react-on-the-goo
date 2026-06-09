69f29731a014010032d9ffc58d852d37e64195a0
Github3121 <mdsani3121@gmail.com>
2026-06-09 11:03:02 +0600

    3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+// import { use } from 'react';
+import { Suspense } from 'react';
 import './App.css'
 import Countries from './components/countries/Countries'
 
 
 function App() {
+  const fetchData = fetch('https://openapi.programming-hero.com/api/all').then((res) => res.json());
   return (
     <>
-      <Countries></Countries>
+      <Suspense fallback={<h1 className="countries-title">Loading...</h1>}>
+        <Countries fetchData={fetchData}></Countries>
+      </Suspense>
     </>
   )
 }
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,10 +1,15 @@
 // import React from 'react';
+import { use } from 'react';
 import '../../App.css';
 
-const Countries = () => {
+const Countries = ({fetchData}) => {
+    const useData = use(fetchData);
+    const countries = useData.countries;
+    console.log(countries);
     return (
         <>
             <h1 className="countries-title">Countries</h1>
+            <p className='lengthShow'>{countries.length} countries found.</p>
         </>
     );
 };
